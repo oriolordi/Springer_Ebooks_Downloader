661b4666c6be00b2335f3b21bd99461b3f2d371b
Oriol Ordi <oriol.ordi@gmail.com>
2020-09-09 13:42:26 +0200

    changed the file opening to a with statement and a function inside it to avoid problems with the document being left open

--- Springer_Ebooks_Downloader.py
+++ Springer_Ebooks_Downloader.py
@@ -18,11 +18,45 @@ import requests
 pdf_path = 'Springer Ebooks.pdf'
 
 
+# Function to process the pdf and get a list of text from it
+def process_pdf(pdf_file):
+    # Read the pdf from the file
+    read_pdf = PyPDF2.PdfFileReader(pdf_file)
+
+    # Get the number of pages from the pdf
+    number_of_pages = read_pdf.getNumPages()
+
+    # Read all the pages
+    pages = []
+    for page_number in range(number_of_pages):
+        pages.append(read_pdf.getPage(page_number))
+
+    # Extract text from the pages
+    text = []
+    for page in pages:
+        text.append(page.extractText())
+
+    # Split the text at every line skip (at every '\n')
+    text_split = []
+    for t in text:
+        text_split.append(t.split('\n'))
+
+    # Convert the list of lists into a single list (or a flat list)
+    text_split_flat = []
+    for list_text in text_split:
+        for item in list_text:
+            text_split_flat.append(item)
+
+    # Return the text_split_flat list
+    return text_split_flat
+
+
 # Get pdf file
 try:
-    pdf_file = open(pdf_path, 'rb')
+    with open(pdf_path, 'rb') as pdf_file:
+        text_split_flat = process_pdf(pdf_file)
 except:
-    print('Error: File "Springer Ebooks.pdf" must be in this directory with that name')
+    print('Error: File "Springer Ebooks.pdf" not found')
     print('\n')
     input('Press enter key to quit')
 
@@ -31,39 +65,6 @@ except:
 print('Starting to download the Springer Ebooks...')
 print('Warning: The books will be saved as pdf files in the same folder that the program was launched from')
 print('\n')
-
-
-# Read the pdf from the file
-read_pdf = PyPDF2.PdfFileReader(pdf_file)
-
-
-# Get the number of pages from the pdf
-number_of_pages = read_pdf.getNumPages()
-
-
-# Read all the pages
-pages = []
-for page_number in range(number_of_pages):
-    pages.append(read_pdf.getPage(page_number))
-
-
-# Extract text from the pages
-text = []
-for page in pages:
-    text.append(page.extractText())
-
-
-# Split the text at every line skip (at every '\n')
-text_split = []
-for t in text:
-    text_split.append(t.split('\n'))
-
-
-# Convert the list of lists into a single list (or a flat list)
-text_split_flat = []
-for list_text in text_split:
-    for item in list_text:
-        text_split_flat.append(item)
 
 
 # Loop the list searching for each book number and saving the book name and link
